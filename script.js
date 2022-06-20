a0f6b8fa7312b51b2847dcde0634f478e0273bc9
Samrebilas <sam.rebilas@gmail.com>
2022-06-20 15:32:49 -0400

    console log random characters from each requirement

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialcharacters = ["!", "#", "$", "%","^", "&", "*", "(", ")", "-", "_", "=", "+"];
     
-function calllowercase(lowercase){
-    return lowercase;
-}
+
+
   
 
 
@@ -17,32 +17,42 @@ function generatePassword(){
   console.log(length);
   if (length < 8 || length > 128){
     alert("Password must be between 8 and 128 characters!");
+   
     return generatePassword();
   }
 var islowercase = window.confirm("Would you like to include lowercase in your password?");{
-    calllowercase();
+    
 }
+var random = Math.floor(Math.random() * lowercase.length)
 
-
-console.log(islowercase)
+console.log(random, lowercase[random]);
 
 
 var isuppercase = window.confirm("Would you like to include uppercase in your password?")
 
-console.log(isuppercase)
+var random = Math.floor(Math.random() * uppercase.length)
+
+console.log(random, uppercase[random]);
+
 
 var isnumeric = window.confirm("Would you like to include numeric values in your password?")
 
-console.log(isnumeric)
+var random = Math.floor(Math.random() * numeric.length)
+
+console.log(random, numeric[random]);
 
 
 var isspecialcharacters = window.confirm("would you like to include special characters?")
 
-console.log(isspecialcharacters)
+var random = Math.floor(Math.random() * specialcharacters.length)
 
+console.log(random, specialcharacters[random]);
 
+for(var i = 0; i < length; i++){
+    
+}
 
-  return 'password';
+  return lowercase, uppercase, numeric, length;
 }
 
 
